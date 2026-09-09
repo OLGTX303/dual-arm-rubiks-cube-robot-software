51ceb48c9d00993ef7b03698ac6c07bdba6b0f88
BOYU <sbse49934@gmail.com>
2026-09-09 17:52:14 +0800

    Restore original arm finger coupling logic

--- cube_motion.py
+++ cube_motion.py
@@ -408,8 +408,7 @@ class MotionCtrl:
         # 存在齿轮，所以需要和手臂电机旋转方向相反，转速绝对值相同，才能保证相对静止
         # 手指目标位置 = finger_zero + arm_offset / 2 + finger_offset
         # 计算手指电机的目标位置
-        finger_sign = 1 if index == 0 else -1
-        finger_target = self.finger_zero[index] + finger_sign * self.arm_offset[index] // 2 + self.finger_offset[index]
+        finger_target = self.finger_zero[index] + self.arm_offset[index] // 2 + self.finger_offset[index]
 
         cmd_trap(self.ser, id_list, False, 
                  [[finger_target, 0, speed, accel, finger_current], 
